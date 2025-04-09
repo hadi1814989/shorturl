804b5fcbd3aa02b71b9af9a6d0f06688b69a9c1c
hadi1814989 <freelancer1814989@gmail.comgit config --global user.email freelancer1814989@gmail.com>
2025-04-09 22:27:45 +0600

    add

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { app } from "./src/app.js";
-import { APP_URL, PORT } from "./src/contants.js";
+import { APP_URL, PORT } from "./src/constants.js";
+
 app.listen(PORT, () => {
-    console.log(`server is running ${APP_URL}:${PORT}`)
-})
+  console.log(`Server is running at ${APP_URL}:${PORT}`);
+});
